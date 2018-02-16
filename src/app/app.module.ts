73fd9a7048cd4c7d45e3792293c73947994a93e6
Sant'Clear <santclear@gmail.com>
2018-02-16 01:02:23 -0200

    Listando categorias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
@@ -6,6 +7,7 @@ import { MyApp } from './app.component';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { CategoriaService } from '../services/domain/categoria.service';
 
 @NgModule({
 	/* TODO: 1. Todas as páginas serão carregas quando a aplicação subir. 
@@ -16,6 +18,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 	],
 	imports: [
 		BrowserModule,
+		HttpClientModule,
 		IonicModule.forRoot(MyApp),
 	],
 	bootstrap: [IonicApp],
@@ -28,7 +31,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 	providers: [
 		StatusBar,
 		SplashScreen,
-		{ provide: ErrorHandler, useClass: IonicErrorHandler }
+		{ provide: ErrorHandler, useClass: IonicErrorHandler },
+		CategoriaService
 	]
 })
 export class AppModule { }
--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CategoriaService } from '../../services/domain/categoria.service';
 
 @IonicPage()
 @Component({
-  selector: 'page-categorias',
-  templateUrl: 'categorias.html',
+	selector: 'page-categorias',
+	templateUrl: 'categorias.html',
 })
 export class CategoriasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	constructor(
+		public navCtrl: NavController,
+		public navParams: NavParams,
+		public categoriaService: CategoriaService) {
+	}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriasPage');
-  }
-
+	ionViewDidLoad() {
+		this.categoriaService.findAll()
+			.subscribe(response => {
+				console.log(response);
+			}, error => {
+				console.log(error);
+			});
+	}
 }
